7dec1da0055e36b00cdde5b577c5e463f7b122dd
ThePhenix57 <thephenix5759@gmail.com>
2026-05-27 03:13:20 +0200

    Update config.js

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,1 +1,10 @@
-
+exports.handler = async () => {
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseKey: process.env.SUPABASE_ANON_KEY
+    })
+  };
+};
